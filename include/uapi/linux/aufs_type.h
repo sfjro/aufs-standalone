78ac643d069e62c4eaf8abc7f6a3a6eb1cb86b21
J. R. Okajima <hooanon05g@gmail.com>
2015-03-30 07:48:44 +0900

    aufs: intro, public header
    A header file for both of kernelspace and userspace.
    
    For the new file fs/aufs/Kconfig, the maximum number of branches is
    customizable, and it determines the type (size) of 'aufs_bindex_t.' The
    type is always 'signed.' If we made it 'unsigned,' then more branches
    would be available. But generally I think 127 (default) is enough and it
    won't be a big issue.
    
    For those who wants more than 127 branches, other values are
    available. But we should care the size of the internal pointer arrays,
    and it is good for the performance to keep it in a page at most.
    AUFS_BRANCH_MAX_511 is mainly for 64bit systems which limits the
    internal array size less than 4k (511 x 8bytes < 4k). Similarly for
    32bit systems, AUFS_BRANCH_MAX_1023 (1023 x 4 bytes < 4k).
    
    See also the documents in this commit.
    
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

---
+++ include/uapi/linux/aufs_type.h
@@ -0,0 +1,57 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2005-2019 Junjiro R. Okajima
+ */
+
+#ifndef __AUFS_TYPE_H__
+#define __AUFS_TYPE_H__
+
+#define AUFS_NAME	"aufs"
+
+#ifdef __KERNEL__
+/*
+ * define it before including all other headers.
+ * sched.h may use pr_* macros before defining "current", so define the
+ * no-current version first, and re-define later.
+ */
+#define pr_fmt(fmt)	AUFS_NAME " %s:%d: " fmt, __func__, __LINE__
+#include <linux/sched.h>
+#undef pr_fmt
+#define pr_fmt(fmt) \
+		AUFS_NAME " %s:%d:%.*s[%d]: " fmt, __func__, __LINE__, \
+		(int)sizeof(current->comm), current->comm, current->pid
+#else
+#include <stdint.h>
+#include <sys/types.h>
+#endif /* __KERNEL__ */
+
+#include <linux/limits.h>
+
+#define AUFS_VERSION	"5.x-rcN"
+
+/* todo? move this to linux-2.6.19/include/magic.h */
+#define AUFS_SUPER_MAGIC	('a' << 24 | 'u' << 16 | 'f' << 8 | 's')
+
+/* ---------------------------------------------------------------------- */
+
+#ifdef CONFIG_AUFS_BRANCH_MAX_127
+typedef int8_t aufs_bindex_t;
+#define AUFS_BRANCH_MAX 127
+#else
+typedef int16_t aufs_bindex_t;
+#ifdef CONFIG_AUFS_BRANCH_MAX_511
+#define AUFS_BRANCH_MAX 511
+#elif defined(CONFIG_AUFS_BRANCH_MAX_1023)
+#define AUFS_BRANCH_MAX 1023
+#elif defined(CONFIG_AUFS_BRANCH_MAX_32767)
+#define AUFS_BRANCH_MAX 32767
+#endif
+#endif
+
+#ifdef __KERNEL__
+#ifndef AUFS_BRANCH_MAX
+#error unknown CONFIG_AUFS_BRANCH_MAX value
+#endif
+#endif /* __KERNEL__ */
+
+#endif /* __AUFS_TYPE_H__ */
