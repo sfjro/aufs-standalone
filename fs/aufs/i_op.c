f0acc2461618244a6a76f6e01b0a643dc9142135
J. R. Okajima <hooanon05g@gmail.com>
2015-03-30 07:49:30 +0900

    aufs: file op
    Implement several f_op functions for non-dir.
    
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

--- fs/aufs/i_op.c
+++ fs/aufs/i_op.c
@@ -744,7 +744,6 @@ static int aufs_setattr(struct user_namespace *userns, struct dentry *dentry,
 		goto out_kfree;
 
 	if (ia->ia_valid & ATTR_FILE) {
-#if 0 /* re-commit later */
 		/* currently ftruncate(2) only */
 		AuDebugOn(!d_is_reg(dentry));
 		file = ia->ia_file;
@@ -754,10 +753,6 @@ static int aufs_setattr(struct user_namespace *userns, struct dentry *dentry,
 			goto out_si;
 		ia->ia_file = au_hf_top(file);
 		a->udba = AuOpt_UDBA_NONE;
-#else
-		err = -ENOSYS;
-		goto out_si;
-#endif
 	} else {
 		/* fchmod() doesn't pass ia_file */
 		a->udba = au_opt_udba(sb);
@@ -795,7 +790,6 @@ static int aufs_setattr(struct user_namespace *userns, struct dentry *dentry,
 	}
 
 	if (ia->ia_valid & ATTR_SIZE) {
-#if 0 /* re-commit later */
 		struct file *f;
 
 		if (ia->ia_size < i_size_read(inode))
@@ -808,9 +802,6 @@ static int aufs_setattr(struct user_namespace *userns, struct dentry *dentry,
 		inode_unlock(a->h_inode);
 		err = vfsub_trunc(&a->h_path, ia->ia_size, ia->ia_valid, f);
 		inode_lock_nested(a->h_inode, AuLsc_I_CHILD);
-#else
-		err = -ENOSYS;
-#endif
 	} else {
 		delegated = NULL;
 		while (1) {
