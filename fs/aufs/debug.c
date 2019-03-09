sfjro/aufs4-linux

aufs: white-out 1/2
The whiteout represents a logical deletion.
Although the document in this commit mentioned about rmdir(2) and
rename(2) for dir, this commit doesn't contain such functions. They will
be added in later commits.

Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

fs/aufs/debug.c
@@ -152,9 +152,9 @@ void au_dpri_dentry(struct dentry *dentry)
 	dinfo = au_di(dentry);
 	if (!dinfo)
 		return;
-	dpri("d-1: bstart %d, bend %d, gen %d\n",
+	dpri("d-1: bstart %d, bend %d, bwh %d, gen %d\n",
 	     dinfo->di_bstart, dinfo->di_bend,
-	     au_digen(dentry));
+	     dinfo->di_bwh, au_digen(dentry));
 	if (dinfo->di_bstart < 0)
 		return;
 	hdp = dinfo->di_hdentry;

fs/aufs/dentry.h
@@ -36,7 +36,7 @@ struct au_dinfo {
 	atomic_t		di_generation;
 
 	struct au_rwsem		di_rwsem;
-	aufs_bindex_t		di_bstart, di_bend;
+	aufs_bindex_t		di_bstart, di_bend, di_bwh;
 	struct au_hdentry	*di_hdentry;
 } ____cacheline_aligned_in_smp;
 
@@ -150,6 +150,12 @@ static inline aufs_bindex_t au_dbend(struct dentry *dentry)
 	return au_di(dentry)->di_bend;
 }
 
+static inline aufs_bindex_t au_dbwh(struct dentry *dentry)
+{
+	DiMustAnyLock(dentry);
+	return au_di(dentry)->di_bwh;
+}
+
 /* todo: hard/soft set? */
 static inline void au_set_dbstart(struct dentry *dentry, aufs_bindex_t bindex)
 {
@@ -161,6 +167,13 @@ static inline void au_set_dbend(struct dentry *dentry, aufs_bindex_t bindex)
 {
 	DiMustWriteLock(dentry);
 	au_di(dentry)->di_bend = bindex;
+}
+
+static inline void au_set_dbwh(struct dentry *dentry, aufs_bindex_t bindex)
+{
+	DiMustWriteLock(dentry);
+	/* dbwh can be outside of bstart - bend range */
+	au_di(dentry)->di_bwh = bindex;
 }
 
 #endif /* __KERNEL__ */

sfjro/aufs-standalone

fs/aufs/debug.c
@@ -138,9 +138,9 @@ void au_dpri_dentry(struct dentry *dentry)
 	dinfo = au_di(dentry);
 	if (!dinfo)
 		return;
-	dpri("d-1: btop %d, bbot %d, gen %d\n",
+	dpri("d-1: btop %d, bbot %d, bwh %d, gen %d\n",
 	     dinfo->di_btop, dinfo->di_bbot,
-	     au_digen(dentry));
+	     dinfo->di_bwh, au_digen(dentry));
 	if (dinfo->di_btop < 0)
 		return;
 	for (bindex = dinfo->di_btop; bindex <= dinfo->di_bbot; bindex++)

fs/aufs/dentry.h
@@ -24,7 +24,7 @@ struct au_dinfo {
 	atomic_t		di_generation;
 
 	struct au_rwsem		di_rwsem;
-	aufs_bindex_t		di_btop, di_bbot;
+	aufs_bindex_t		di_btop, di_bbot, di_bwh;
 	struct au_hdentry	*di_hdentry;
 	struct rcu_head		rcu;
 } ____cacheline_aligned_in_smp;
@@ -147,6 +147,12 @@ static inline aufs_bindex_t au_dbbot(struct dentry *dentry)
 	return au_di(dentry)->di_bbot;
 }
 
+static inline aufs_bindex_t au_dbwh(struct dentry *dentry)
+{
+	DiMustAnyLock(dentry);
+	return au_di(dentry)->di_bwh;
+}
+
 /* todo: hard/soft set? */
 static inline void au_set_dbtop(struct dentry *dentry, aufs_bindex_t bindex)
 {
@@ -158,6 +164,13 @@ static inline void au_set_dbbot(struct dentry *dentry, aufs_bindex_t bindex)
 {
 	DiMustWriteLock(dentry);
 	au_di(dentry)->di_bbot = bindex;
+}
+
+static inline void au_set_dbwh(struct dentry *dentry, aufs_bindex_t bindex)
+{
+	DiMustWriteLock(dentry);
+	/* dbwh can be outside of btop - bbot range */
+	au_di(dentry)->di_bwh = bindex;
 }
 
 #endif /* __KERNEL__ */