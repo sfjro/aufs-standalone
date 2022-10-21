sfjro/aufs4-linux

aufs: infos generation 2/2
The generation of iinfo and dinfo inherit sbinfo's.
Also iinfo generation tracks the branch inode's generation to test the
matching after the branch management.

Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

fs/aufs/debug.c
@@ -103,8 +103,8 @@ void au_dpri_inode(struct inode *inode)
 	iinfo = au_ii(inode);
 	if (!iinfo)
 		return;
-	dpri("i-1: bstart %d, bend %d\n",
-	     iinfo->ii_bstart, iinfo->ii_bend);
+	dpri("i-1: bstart %d, bend %d, gen %d\n",
+	     iinfo->ii_bstart, iinfo->ii_bend, au_iigen(inode, NULL));
 	if (iinfo->ii_bstart < 0)
 		return;
 	for (bindex = iinfo->ii_bstart; bindex <= iinfo->ii_bend; bindex++)
@@ -152,8 +152,9 @@ void au_dpri_dentry(struct dentry *dentry)
 	dinfo = au_di(dentry);
 	if (!dinfo)
 		return;
-	dpri("d-1: bstart %d, bend %d\n",
-	     dinfo->di_bstart, dinfo->di_bend);
+	dpri("d-1: bstart %d, bend %d, gen %d\n",
+	     dinfo->di_bstart, dinfo->di_bend,
+	     au_digen(dentry));
 	if (dinfo->di_bstart < 0)
 		return;
 	hdp = dinfo->di_hdentry;
@@ -195,4 +196,24 @@ void __au_dbg_verify_dinode(struct dentry *dentry, const char *func, int line)
 			BUG();
 		}
 	}
+}
+
+void au_dbg_verify_gen(struct dentry *parent, unsigned int sigen)
+{
+	int err, i, j;
+	struct au_dcsub_pages dpages;
+	struct au_dpage *dpage;
+	struct dentry **dentries;
+
+	err = au_dpages_init(&dpages, GFP_NOFS);
+	AuDebugOn(err);
+	err = au_dcsub_pages_rev_aufs(&dpages, parent, /*do_include*/1);
+	AuDebugOn(err);
+	for (i = dpages.ndpage - 1; !err && i >= 0; i--) {
+		dpage = dpages.dpages + i;
+		dentries = dpage->dentries;
+		for (j = dpage->ndentry - 1; !err && j >= 0; j--)
+			AuDebugOn(au_digen_test(dentries[j], sigen));
+	}
+	au_dpages_free(&dpages);
 }

sfjro/aufs-standalone

fs/aufs/debug.c
@@ -90,8 +90,8 @@ void au_dpri_inode(struct inode *inode)
 		return;
 
 	iinfo = au_ii(inode);
-	dpri("i-1: btop %d, bbot %d\n",
-	     iinfo->ii_btop, iinfo->ii_bbot);
+	dpri("i-1: btop %d, bbot %d, gen %d\n",
+	     iinfo->ii_btop, iinfo->ii_bbot, au_iigen(inode));
 	if (iinfo->ii_btop < 0)
 		return;
 	for (bindex = iinfo->ii_btop; bindex <= iinfo->ii_bbot; bindex++)
@@ -138,8 +138,9 @@ void au_dpri_dentry(struct dentry *dentry)
 	dinfo = au_di(dentry);
 	if (!dinfo)
 		return;
-	dpri("d-1: btop %d, bbot %d\n",
-	     dinfo->di_btop, dinfo->di_bbot);
+	dpri("d-1: btop %d, bbot %d, gen %d\n",
+	     dinfo->di_btop, dinfo->di_bbot,
+	     au_digen(dentry));
 	if (dinfo->di_btop < 0)
 		return;
 	for (bindex = dinfo->di_btop; bindex <= dinfo->di_bbot; bindex++)
@@ -180,4 +181,24 @@ void __au_dbg_verify_dinode(struct dentry *dentry, const char *func, int line)
 			BUG();
 		}
 	}
+}
+
+void au_dbg_verify_gen(struct dentry *parent, unsigned int sigen)
+{
+	int err, i, j;
+	struct au_dcsub_pages dpages;
+	struct au_dpage *dpage;
+	struct dentry **dentries;
+
+	err = au_dpages_init(&dpages, GFP_NOFS);
+	AuDebugOn(err);
+	err = au_dcsub_pages_rev_aufs(&dpages, parent, /*do_include*/1);
+	AuDebugOn(err);
+	for (i = dpages.ndpage - 1; !err && i >= 0; i--) {
+		dpage = dpages.dpages + i;
+		dentries = dpage->dentries;
+		for (j = dpage->ndentry - 1; !err && j >= 0; j--)
+			AuDebugOn(au_digen_test(dentries[j], sigen));
+	}
+	au_dpages_free(&dpages);
 }