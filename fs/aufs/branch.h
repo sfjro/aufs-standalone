sfjro/aufs4-linux

aufs: remount 4/5, refresh the internal branch array
Maintain the internal array including corresponding XINO file and sysfs
entries.

Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

fs/aufs/branch.h
@@ -113,7 +113,7 @@ struct au_sbinfo;
 void au_br_free(struct au_sbinfo *sinfo);
 int au_br_index(struct super_block *sb, aufs_bindex_t br_id);
 struct au_opt_add;
-int au_br_add(struct super_block *sb, struct au_opt_add *add);
+int au_br_add(struct super_block *sb, struct au_opt_add *add, int remount);
 
 /* xino.c */
 static const loff_t au_loff_max = LLONG_MAX;
@@ -134,7 +134,7 @@ int au_xino_br(struct super_block *sb, struct au_branch *br, ino_t hino,
 	       struct file *base_file, int do_test);
 
 struct au_opt_xino;
-int au_xino_set(struct super_block *sb, struct au_opt_xino *xino);
+int au_xino_set(struct super_block *sb, struct au_opt_xino *xino, int remount);
 void au_xino_clr(struct super_block *sb);
 struct file *au_xino_def(struct super_block *sb);
 int au_xino_path(struct seq_file *seq, struct file *file);

fs/aufs/sysfs.c
@@ -208,6 +208,29 @@ void sysaufs_br_init(struct au_branch *br)
 	}
 }
 
+void sysaufs_brs_del(struct super_block *sb, aufs_bindex_t bindex)
+{
+	struct au_branch *br;
+	struct kobject *kobj;
+	struct au_brsysfs *br_sysfs;
+	int i;
+	aufs_bindex_t bend;
+
+	if (!sysaufs_brs)
+		return;
+
+	kobj = &au_sbi(sb)->si_kobj;
+	bend = au_sbend(sb);
+	for (; bindex <= bend; bindex++) {
+		br = au_sbr(sb, bindex);
+		br_sysfs = br->br_sysfs;
+		for (i = 0; i < ARRAY_SIZE(br->br_sysfs); i++) {
+			sysfs_remove_file(kobj, &br_sysfs->attr);
+			br_sysfs++;
+		}
+	}
+}
+
 void sysaufs_brs_add(struct super_block *sb, aufs_bindex_t bindex)
 {
 	int err, i;

fs/aufs/xino.c
@@ -875,10 +875,12 @@ void au_xino_clr(struct super_block *sb)
 	au_opt_clr(sbinfo->si_mntflags, XINO);
 }
 
-int au_xino_set(struct super_block *sb, struct au_opt_xino *xino)
+int au_xino_set(struct super_block *sb, struct au_opt_xino *xino, int remount)
 {
-	int err;
-	struct dentry *parent;
+	int err, skip;
+	struct dentry *parent, *cur_parent;
+	struct qstr *dname, *cur_name;
+	struct file *cur_xino;
 	struct inode *dir;
 	struct au_sbinfo *sbinfo;
 
@@ -887,6 +889,20 @@ int au_xino_set(struct super_block *sb, struct au_opt_xino *xino)
 	err = 0;
 	sbinfo = au_sbi(sb);
 	parent = dget_parent(xino->file->f_path.dentry);
+	if (remount) {
+		skip = 0;
+		dname = &xino->file->f_path.dentry->d_name;
+		cur_xino = sbinfo->si_xib;
+		if (cur_xino) {
+			cur_parent = dget_parent(cur_xino->f_path.dentry);
+			cur_name = &cur_xino->f_path.dentry->d_name;
+			skip = (cur_parent == parent
+				&& au_qstreq(dname, cur_name));
+			dput(cur_parent);
+		}
+		if (skip)
+			goto out;
+	}
 
 	au_opt_set(sbinfo->si_mntflags, XINO);
 	dir = parent->d_inode;

sfjro/aufs-standalone

fs/aufs/branch.h
@@ -182,7 +182,7 @@ struct au_sbinfo;
 void au_br_free(struct au_sbinfo *sinfo);
 int au_br_index(struct super_block *sb, aufs_bindex_t br_id);
 struct au_opt_add;
-int au_br_add(struct super_block *sb, struct au_opt_add *add);
+int au_br_add(struct super_block *sb, struct au_opt_add *add, int remount);
 
 /* xino.c */
 static const loff_t au_loff_max = LLONG_MAX;
@@ -216,7 +216,7 @@ struct file *au_xino_file1(struct au_xino *xi);
 
 struct au_opt_xino;
 void au_xino_clr(struct super_block *sb);
-int au_xino_set(struct super_block *sb, struct au_opt_xino *xiopt);
+int au_xino_set(struct super_block *sb, struct au_opt_xino *xiopt, int remount);
 struct file *au_xino_def(struct super_block *sb);
 int au_xino_init_br(struct super_block *sb, struct au_branch *br, ino_t hino,
 		    struct path *base);

fs/aufs/sysfs.c
@@ -198,6 +198,29 @@ void sysaufs_br_init(struct au_branch *br)
 	}
 }
 
+void sysaufs_brs_del(struct super_block *sb, aufs_bindex_t bindex)
+{
+	struct au_branch *br;
+	struct kobject *kobj;
+	struct au_brsysfs *br_sysfs;
+	int i;
+	aufs_bindex_t bbot;
+
+	if (!sysaufs_brs)
+		return;
+
+	kobj = &au_sbi(sb)->si_kobj;
+	bbot = au_sbbot(sb);
+	for (; bindex <= bbot; bindex++) {
+		br = au_sbr(sb, bindex);
+		br_sysfs = br->br_sysfs;
+		for (i = 0; i < ARRAY_SIZE(br->br_sysfs); i++) {
+			sysfs_remove_file(kobj, &br_sysfs->attr);
+			br_sysfs++;
+		}
+	}
+}
+
 void sysaufs_brs_add(struct super_block *sb, aufs_bindex_t bindex)
 {
 	int err, i;

fs/aufs/xino.c
@@ -1536,12 +1536,14 @@ void au_xino_clr(struct super_block *sb)
 	au_opt_clr(sbinfo->si_mntflags, XINO);
 }
 
-int au_xino_set(struct super_block *sb, struct au_opt_xino *xiopt)
+int au_xino_set(struct super_block *sb, struct au_opt_xino *xiopt, int remount)
 {
-	int err;
-	struct dentry *dentry, *parent;
+	int err, skip;
+	struct dentry *dentry, *parent, *cur_dentry, *cur_parent;
+	struct qstr *dname, *cur_name;
+	struct file *cur_xino;
 	struct au_sbinfo *sbinfo;
-	struct path *path;
+	struct path *path, *cur_path;
 
 	SiMustWriteLock(sb);
 
@@ -1550,6 +1552,23 @@ int au_xino_set(struct super_block *sb, struct au_opt_xino *xiopt)
 	path = &xiopt->file->f_path;
 	dentry = path->dentry;
 	parent = dget_parent(dentry);
+	if (remount) {
+		skip = 0;
+		cur_xino = sbinfo->si_xib;
+		if (cur_xino) {
+			cur_path = &cur_xino->f_path;
+			cur_dentry = cur_path->dentry;
+			cur_parent = dget_parent(cur_dentry);
+			cur_name = &cur_dentry->d_name;
+			dname = &dentry->d_name;
+			skip = (cur_parent == parent
+				&& au_qstreq(dname, cur_name));
+			dput(cur_parent);
+		}
+		if (skip)
+			goto out;
+	}
+
 	au_opt_set(sbinfo->si_mntflags, XINO);
 	err = au_xino_set_xib(sb, path);
 	/* si_x{read,write} are set */