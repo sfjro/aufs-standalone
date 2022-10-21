sfjro/aufs4-linux

aufs: finfo core
The structure is very similar to iinfo and dinfo (in previous commits).
This commit is for non-dir files. For a directory, see later commit.

Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

fs/aufs/module.c
@@ -48,6 +48,9 @@ static int __init au_cache_init(void)
 		au_cachep[AuCache_ICNTNR] = AuCacheCtor(au_icntnr,
 							au_icntnr_init_once);
 	if (au_cachep[AuCache_ICNTNR])
+		au_cachep[AuCache_FINFO] = AuCacheCtor(au_finfo,
+						       au_fi_init_once);
+	if (au_cachep[AuCache_FINFO])
 		return 0;
 
 	return -ENOMEM;

sfjro/aufs-standalone

fs/aufs/module.c
@@ -98,6 +98,9 @@ static int __init au_cache_init(void)
 		au_cache[AuCache_ICNTNR] = AuCacheCtor(au_icntnr,
 						       au_icntnr_init_once);
 	if (au_cache[AuCache_ICNTNR])
+		au_cache[AuCache_FINFO] = AuCacheCtor(au_finfo,
+						      au_fi_init_once);
+	if (au_cache[AuCache_FINFO])
 		return 0;
 
 	au_cache_fin();