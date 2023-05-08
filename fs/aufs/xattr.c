fc3a0bd55cedbe420540a2d77c5a8c7e51781993
J. R. Okajima <hooanon05g@gmail.com>
2023-05-08 19:33:50 +0900

    aufs: linux-v6.4-rc1, remove posix acl handlers
    Simply follows the commit in mainline,
    	0c95c025a02e 2023-03-06 fs: drop unused posix acl handlers
    
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

--- fs/aufs/xattr.c
+++ fs/aufs/xattr.c
@@ -8,7 +8,6 @@
  */
 
 #include <linux/fs.h>
-#include <linux/posix_acl_xattr.h>
 #include <linux/xattr.h>
 #include "aufs.h"
 
@@ -338,10 +337,6 @@ static const struct xattr_handler au_xattr_handler = {
 };
 
 static const struct xattr_handler *au_xattr_handlers[] = {
-#ifdef CONFIG_FS_POSIX_ACL
-	&posix_acl_access_xattr_handler,
-	&posix_acl_default_xattr_handler,
-#endif
 	&au_xattr_handler, /* must be last */
 	NULL
 };
